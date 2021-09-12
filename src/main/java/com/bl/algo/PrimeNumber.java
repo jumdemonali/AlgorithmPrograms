504d5a6c0e549f02de1e84db912bf4e48049e9a7
jumdemonali <jumdemonali@gmail.com>
2021-09-12 20:30:49 +0530

    Added: Prime numbers in range of 0-1000

---
+++ src/main/java/com/bl/algo/PrimeNumber.java
@@ -0,0 +1,27 @@
+package com.bl.algo;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        int n1 = 1, n2 = 1000;
+        primeNumbers(n1, n2);
+    }
+
+    private static void primeNumbers(int a, int b) {
+        int flag;
+        System.out.println("Prime numbers are:");
+        for (int i = a; i <= b; i++) {
+            if (i == 1 || i == 0)
+                continue;
+            flag = 1;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 1)
+                System.out.print(i + " ");
+        }
+    }
+}
