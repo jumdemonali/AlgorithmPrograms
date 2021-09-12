fda08ca4835187908bbdd0d931d6d2a83ae92e7d
jumdemonali <jumdemonali@gmail.com>
2021-09-12 19:11:28 +0530

    Added: Bubble sort program

--- src/main/java/com/bl/algo/BubbleSort.java
+++ src/main/java/com/bl/algo/BubbleSort.java
@@ -1,37 +1,39 @@
 package com.bl.algo;
-import java.util.*;
-public class BubbleSort {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            List<Integer> list = new ArrayList<Integer>();
-            System.out.println("Enter the size of the List");
-            int size = scanner.nextInt();
-            System.out.println("Enter Values into the List ");
-            for (int i = 0; i < size; i++) {
-                int input = scanner.nextInt();
-                list.add(input);
-            }
-            Integer[] intArray = new Integer[list.size()];
-            intArray = list.toArray(intArray);
-            bubbleSort(intArray);
-            for (int i = 0; i < intArray.length; i++) {
-                System.out.print(+intArray[i]+" ");
-            }
-        }
 
-        private static void bubbleSort(Integer[] intArray) {
-            int length = intArray.length;
-            for (int i = 0; i < length; i++) {
-                for (int j = i + 1; j < length; j++) {
-                    if (intArray[i] > intArray[j]) {
-                        int temp = intArray[i];
-                        intArray[i] = intArray[j];
-                        intArray[j] = temp;
-                    }
+import java.util.*;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> list = new ArrayList<Integer>();
+        System.out.println("Enter the size of the List");
+        int size = scanner.nextInt();
+        System.out.println("Enter Values into the List ");
+        for (int i = 0; i < size; i++) {
+            int input = scanner.nextInt();
+            list.add(input);
+        }
+        Integer[] intArray = new Integer[list.size()];
+        intArray = list.toArray(intArray);
+        bubbleSort(intArray);
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print(+intArray[i] + " ");
+        }
+    }
+
+    private static void bubbleSort(Integer[] intArray) {
+        int length = intArray.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                if (intArray[i] > intArray[j]) {
+                    int temp = intArray[i];
+                    intArray[i] = intArray[j];
+                    intArray[j] = temp;
                 }
             }
-
         }
+
+    }
 
 
 }
